c37c6a01343544dc5d8d496bb4f5e1e6758a2c95
OrigamiK linux <korigamik@gmail.com>
2021-06-19 21:15:03 +0530

    initial commit

---
+++ Hatimitsuo/download.py
@@ -0,0 +1,49 @@
+import aiofiles
+import asyncio
+from os import listdir, getcwd
+from os.path import join, isfile
+from typing import List
+from re import search, sub, IGNORECASE, compile
+
+"""
+what I ran first
+youtube-dl --skip-download --write-description https://www.youtube.com/channel/UCWP9fMCl9TkkMfCcLLFmHHw/videos
+"""
+
+
+def get_files(mypath: str) -> List[str]:
+    return [f for f in listdir(mypath) if isfile(join(mypath, f)) and "description" in f]
+
+
+identity = 0
+backlist_pattern = compile('instagram|facebook|twitter|youtube|subscri|alphaco|wallpaper|source', flags=IGNORECASE)
+
+def generate_id():
+    global identity
+    identity += 1
+    return identity
+
+
+async def reader(file_name: str, identity) -> None:
+    async with aiofiles.open(file_name, mode="r") as f:
+        name = file_name.replace(".description", "")
+        name = sub(
+            r"(\({0,1}\([Gg]uit.+)|(\({0,1}[Ss]olo.+)|(\({0,1}[Ff]ingerst.+)|(\[TABS\])",
+            "",
+            name,
+        ).strip()
+        flag = True
+        async for line in f:
+            if 'http' in line and backlist_pattern.search(line) is None:
+                print(f"{identity}. [{name}] ", line.strip())
+                flag = False
+        if flag:
+            print(f"{identity}. [{name}]: No links available")
+
+
+async def parser(file_names: List[str]):
+    tasks = [reader(file_name, generate_id()) for file_name in file_names]
+    await asyncio.gather(*tasks)
+
+
+asyncio.run(parser(get_files(getcwd())))
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# free-guitar-tabs-downloader
+# Free-guitar-tabs-downloader
+
+### This is a tool to download guitar tabs from various artists having various songs. 
+
+    1. The tabs are free as provided by their respective creators.
+
+   2. You can see the folders of artists in the code of this repo. Run the python/ any other language programs by installing the required dependencies to get a new list of links.
+
+   3. You can use the links that are already made inside the folders but beware that they may be outdated.
+
+   4. You can use tools such as [Jdownloader2](https://jdownloader.org/jdownloader2) to batch download the tab files.
+
+   5. To contribute you can create a pull request with a new folder with the name of the artist and the current `links.txt`, the program file or any other required file.
+
+   6. Do not upload the actual files for the tabs! Write a cloud link to download from inside your `links.txt` if you wish to do so.
+
+I hope you enjoy playing the tabs.
