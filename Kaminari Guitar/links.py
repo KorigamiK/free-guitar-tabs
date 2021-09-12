0a9ec8cab4234e3cad980a5dc039e50302af1742
OrigamiK linux <korigamik@gmail.com>
2021-09-12 18:30:58 +0530

    Update for kaminari guitar with less concurrency

--- Kaminari Guitar/links.py
+++ Kaminari Guitar/links.py
@@ -5,24 +5,31 @@ from scraper import scraper
 from aiohttp import ClientSession
 from asyncio import run
 
-LAST_PAGE = 127
+LAST_PAGE = 128
+
 
 def divider(total, part_size):
     s = 1
-    for _ in range(total//part_size):
+    for _ in range(total // part_size):
         e = s + part_size - 1
         yield s, e
         s += part_size
     yield total - (total % part_size) + 1, total
 
+
 async def gather_limitter(*args: coroutine, max=5):
     start = 0
-    while start<len(args):        
-        iterable_range = range(start, start + max) if len(args) >= start+max else range(start, len(args))
+    while start < len(args):
+        iterable_range = (
+            range(start, start + max)
+            if len(args) >= start + max
+            else range(start, len(args))
+        )
         tasks = [args[i] for i in iterable_range]
         await asyncio.gather(*tasks, return_exceptions=True)
-        print(f'Completed {len(tasks) + start} of {len(args)} tasks \n')
+        print(f"Completed {len(tasks) + start} of {len(args)} tasks \n")
         start += max
+
 
 class kaminari:
     page_template = "https://music.kaminari.info/page/{}/"
@@ -41,7 +48,7 @@ class kaminari:
             print("Runtime error")
 
         except Exception as e:
-            print(f'Exception {e} on {link}')
+            print(f"Exception {e} on {link}")
 
     async def get_download_link(self, tabulature_url):
         kaminari.index += 1
@@ -49,28 +56,27 @@ class kaminari:
         # print(f'starting {idx}')
         try:
             page = await self.request.get(tabulature_url)
-            title = page.select_one('.titles > a').text
+            title = page.select_one(".titles > a").text
             ret = page.select_one('[target="_blank"]').get("href")
-            print(f'{idx: 04}. [{title}]({ret})')
+            print(f"{idx: 04}. [{title}]({ret})")
         except Exception as e:
-            print(f'Exception {e} on {tabulature_url}')
+            print(f"Exception {e} on {tabulature_url}")
         # return ret
 
     async def get_all_pages(self):
         def get_list_from_range(start, end):
             return [
-                    self._get_from_page(kaminari.page_template.format(number))
-                    for number in range(start, end)
-                ]
+                self._get_from_page(kaminari.page_template.format(number))
+                for number in range(start, end)
+            ]
 
-        for start, end in divider(LAST_PAGE, 3): # change this
-            for links in asyncio.as_completed(get_list_from_range(start, end+1)):
+        for start, end in divider(LAST_PAGE, 3):  # change this
+            for links in asyncio.as_completed(get_list_from_range(start, end + 1)):
                 links = await links
                 for link in links:
                     yield link
             await asyncio.sleep(5)
             start = end
-
 
     async def get_all_links(self):
         tasks = []
